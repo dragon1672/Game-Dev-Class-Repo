3d85738f6ec0270e3cdaf82c154ea7a22aaca264
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2013-12-10 22:13:52 +0000

    Creating solution for Credit Screen reverting back game solution
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@95 c70e0d21-08ce-ef44-a38d-91c21494a941

---
+++ Credit Screen/Game/TextParticalManager.cpp
@@ -0,0 +1,53 @@
+#include "TextParticalManager.h"
+#include "TempGlobal.h"
+
+void TextParticalManager::initText(Vector2D& pos, const char* text, Font *style, int size, int particalSize) {
+	Vector2D startingPos = pos;//copy memory
+	int charWidth  = LETTER_WIDTH * size*particalSize + paddingBetweenCharacters;
+	int charHeight = LETTER_WIDTH * size*particalSize + paddingBetweenCharacters;
+	for(unsigned int i=0; i < strlen(text); i++) {
+		if(
+		Vector2D charPos = startingPos + Vector2D((float)(i * charWidth),0);
+		style->getCharacter(text[i])->initParticals(startingPos,particalSize,size,this);
+	}
+}
+float mouseRadius = 50;
+void TextParticalManager::update(float dt) {
+	if(Core::Input::IsPressed(Core::Input::BUTTON_LEFT)) mouseRadius  = 500;
+	else if(Core::Input::IsPressed(Core::Input::BUTTON_RIGHT)) mouseRadius = 10;
+	else mouseRadius = 50;
+	Vector2D mouse = Vector2D((float)Core::Input::GetMouseX(),(float)Core::Input::GetMouseY());
+	for(int i=0; i < numOfParticals; i++) {//since all particles are used no lifetime required
+		Vector2D difference = mouse - myParticals[i].pos;
+		if(difference.lengthSquared()<mouseRadius*mouseRadius)
+			myParticals[i].updateVelToPos(myParticals[i].pos-difference);
+		else
+			myParticals[i].updateVelToTarget();
+
+		myParticals[i].update(dt);
+	}
+}
+void TextParticalManager::draw(Core::Graphics& graphics) {
+	for(int i=0; i < numOfParticals; i++) {//since all particles are used no lifetime required
+		graphics.SetColor(RGB(255,255,0));
+		//graphics.SetColor(myParticals[i].color);
+		for(int j=0; j < myParticals[i].size; j++) {
+			Vector2D start = myParticals[i].pos + Vector2D((float)j,0);
+			Vector2D end   = start + Vector2D(0,(float)myParticals[i].size);
+			graphics.DrawLine(start.getX(),start.getY(),end.getX(),end.getY());
+		}
+	}
+}
+#include <random>
+
+void TextParticalManager::initPartical(Vector2D& destinationLocation, int particalSize) {
+	int index = numOfParticals++;
+	myParticals[index].target = destinationLocation;
+#ifdef RANDOM_SPAWN
+	myParticals[index].pos    = Vector2D(rand()%1000,rand()%1000);
+#else
+	myParticals[index].pos    = Vector2D(750,650);
+#endif
+	myParticals[index].color  = textColor;
+	myParticals[index].size   = particalSize;
+}
