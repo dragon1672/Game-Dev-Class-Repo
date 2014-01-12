32b0bb80ffcb4a38cb96b10779ad8c18385dac1d
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-01-12 00:38:30 +0000

    using own shaders
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@116 c70e0d21-08ce-ef44-a38d-91c21494a941

--- OpenGLScratchPad/OpenGLScratchPad/MeGLWindow.cpp
+++ OpenGLScratchPad/OpenGLScratchPad/MeGLWindow.cpp
@@ -1,36 +1,100 @@
-#include <gl/glew.h>
+#include <GL\glew.h>
 #include "MeGLWindow.h"
-#include <glm/glm.hpp>
+#include <Qt/qdebug.h>
 
-typedef glm::vec2 Vector2D;
-typedef glm::vec3 Vector3D;
+char * vertexShaderCode = "#version 400\r\n"
+	""
+	"in layout(location=0) vec2 pos;"
+	"in layout(location=1) vec3 col;"
+	""
+	"out vec3 outColor;"
+	""
+	""
+	"void main() {"
+	"	gl_Position = vec4(pos,1,1);"
+	"	outColor = col;"
+	"}"
+	"";
 
-void MeGLWindow::initializeGL() {
+char * fragShaderCode = "#version 400\r\n"
+	""
+	"in vec3 outColor;"//name must match
+	"out vec3 finalColor;"
+	""
+	"void main() {"
+	"	finalColor = outColor;"
+	"}"
+	"";
+
+void MeGLWindow::initializeGL()
+{
 	glewInit();
+	createTriange();
+	initShaders();
+}
 
-	Vector3D verts[] = {
-		Vector3D(+0.0f, +0.8f, +0.0f),
-		Vector3D(+1.0f, +0.0f, +0.0f),
+void MeGLWindow::loadShader(char * code, GLuint id) {
+	const char * codeAdapt[1];
+	codeAdapt[0] = code;
+	glShaderSource(id,1,codeAdapt,0);
 
-		Vector3D(-0.8f, -0.8f, +0.0f),
-		Vector3D(+0.0f, +1.0f, +0.0f),
+	glCompileShader(id);
+	
+	GLint compileStatus;
+	glGetShaderiv(id,GL_COMPILE_STATUS, &compileStatus);
+	if(compileStatus!= GL_TRUE) {
+		GLint logLength;
+		glGetShaderiv(id,GL_INFO_LOG_LENGTH,&logLength);
+		char * buffer = new char[logLength];
+		GLsizei someRandom;
+		glGetShaderInfoLog(id,logLength,&someRandom,buffer);
+		qDebug() << buffer;
+		delete [] buffer;
+	}
+}
 
-		Vector3D(+0.8f, -0.8f, +0.0f),
-		Vector3D(+0.0f, +0.0f, +1.0f),
+void MeGLWindow::initShaders() {
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	loadShader(vertexShaderCode,vertexShaderID);
+	loadShader(fragShaderCode,fragShaderID);
+
+	GLuint programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragShaderID);
+
+	glLinkProgram(programID);
+
+	glUseProgram(programID);
+}
+void MeGLWindow::createTriange() {
+	GLfloat vertices[] = 
+	{
+		+0.0f, +0.8f,
+		+1.0f, +0.0f, +0.0f,
+
+		-0.8f, -0.8f,
+		+0.0f, +1.0f, +0.0f,
+
+		+0.8f, -0.8f,
+		+0.0f, +0.0f, +1.0f,
 
 	};
+
 	GLuint bufferID;
-	glGenBuffers(1,&bufferID);
+	glGenBuffers(1, &bufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, bufferID);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(Vector3D),0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
 
-	glEnableVertexAttribArray(3);
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 2*sizeof(Vector3D), (void *)(sizeof(Vector3D)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*) (2 * sizeof(GLfloat)));
 }
-void MeGLWindow::paintGL() {
-	glDrawArrays(GL_TRIANGLES,0,3);
+void MeGLWindow::paintGL()
+{
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- OpenGLScratchPad/OpenGLScratchPad/MeGLWindow.h
+++ OpenGLScratchPad/OpenGLScratchPad/MeGLWindow.h
@@ -1,10 +1,16 @@
 #pragma once
+
 #include <QtOpenGL\qglwidget>
 
-class MeGLWindow : public QGLWidget {
+class MeGLWindow : public QGLWidget
+{
+private:
+	void createTriange();
+	void initShaders();
+	void loadShader(char * code, GLuint id);
 protected:
 	void initializeGL();
 	void paintGL();
 public:
-
 };
+
