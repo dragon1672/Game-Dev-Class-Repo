ac9a3f8b149f081183d1ae91fd3d716dea83504b
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-01-15 02:42:55 +0000

    transformation matrix now manpulates image
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@118 c70e0d21-08ce-ef44-a38d-91c21494a941

--- OpenGLScratchPad/OpenGLScratchPad/MeGLWindow.cpp
+++ OpenGLScratchPad/OpenGLScratchPad/MeGLWindow.cpp
@@ -1,6 +1,12 @@
 #include <GL\glew.h>
 #include "MeGLWindow.h"
 #include <Qt/qdebug.h>
+#include <glm/glm.hpp>
+#include "glm/gtx/transform.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+using glm::vec3;
+using glm::mat4x4;
 
 char * vertexShaderCode = "#version 400\r\n"
 	""
@@ -9,10 +15,11 @@ char * vertexShaderCode = "#version 400\r\n"
 	""
 	"out vec3 outColor;"
 	""
+	"uniform mat4x4 transformation;"
 	""
 	"void main() {"
-	"	gl_Position = vec4(pos,1,1);"
-	"	outColor = col;"
+		"gl_Position = transformation * vec4(pos.x,pos.y,1,1);"
+		"outColor = col;"
 	"}"
 	"";
 
@@ -26,7 +33,14 @@ char * fragShaderCode = "#version 400\r\n"
 	"}"
 	"";
 
-
+GLuint programID;
+//mat3x3 transformation;
+vec3 translation;
+const glm::vec4 acc(0,1,0,0);
+float angle;
+const float SPEED = .02f;
+const float SCALE = .5f;
+mat4x4 transformation;
 
 void MeGLWindow::initializeGL()
 {
@@ -79,8 +93,8 @@ void MeGLWindow::initShaders() {
 	blocks[1].id = glCreateShader(GL_FRAGMENT_SHADER);
 	blocks[0].code = vertexShaderCode;
 	blocks[1].code = fragShaderCode;
-
-	glUseProgram(buildProgram(2,blocks,true));
+	programID = buildProgram(2,blocks,true);
+	glUseProgram(programID);
 }
 
 void MeGLWindow::createTriange() {
@@ -110,7 +124,41 @@ void MeGLWindow::createTriange() {
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*) (2 * sizeof(GLfloat)));
 }
 void MeGLWindow::myUpdate() {
+	/*static int frames = 0;
+	static const int frameUpdateRate = 1000;
+	frames++;
+	if(frames%frameUpdateRate==0)
+		qDebug() << "frame: " << frames;
+	//*/
+	const float angleAcc = 2;
 
+	if(GetAsyncKeyState(VK_LEFT)) {
+		angle += angleAcc;
+	} else if(GetAsyncKeyState(VK_RIGHT)) {
+		angle -= angleAcc;
+	}
+	if(GetAsyncKeyState(VK_UP)) {
+		translation += SPEED * vec3(glm::rotate(angle,vec3(0,0,1)) * acc);
+	} else if(GetAsyncKeyState(VK_DOWN)) {
+		translation -= SPEED * vec3(glm::rotate(angle,vec3(0,0,1)) * acc);
+	}
+	
+	
+	if(GetAsyncKeyState(' ')) {
+		qDebug() << "angle: " << angle << " trans: " << translation.x << "," << translation.y;
+	}
+
+
+	transformation = glm::translate(translation);
+	transformation *= glm::rotate(angle,vec3(0,0,1));
+	transformation *= glm::scale(vec3(SCALE,SCALE,1));
+
+
+
+	GLint transformationUniformLocation = glGetUniformLocation(programID,"transformation");
+	glUniformMatrix4fv(transformationUniformLocation,1,false,&transformation[0][0]);
+	glClear(GL_COLOR_BUFFER_BIT);
+	repaint();
 }
 void MeGLWindow::paintGL() {
 	glDrawArrays(GL_TRIANGLES, 0, 3);
