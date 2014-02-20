ee4e7e42d3528b44fb9ce61a22601bf96bbc29d8
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-02-20 17:57:05 +0000

    changed MOC file to dynamically build
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@167 c70e0d21-08ce-ef44-a38d-91c21494a941

--- ModelViewProjection/ModelViewProjection/compile MOC.bat
+++ ModelViewProjection/ModelViewProjection/compile MOC.bat
@@ -1,5 +1,12 @@
-echo creating moc files
-moc48 MyWindow.h > MyWindow_mock.cpp
-moc48 DebugSlider.h > DebugSlider_mock.cpp
-moc48 MyGUI.h > MyGUI_mock.cpp
-echo creating moc files complete
+@echo off
+set ENABLED=false
+
+if %ENABLED% == true (
+	echo creating moc files
+	moc48 MyWindow.h > MyWindow_mock.cpp
+	moc48 DebugSlider.h > DebugSlider_mock.cpp
+	moc48 MyGUI.h > MyGUI_mock.cpp
+	echo creating moc files complete
+) else (
+	echo rebuilding moc files skipped, re-enable in batch file
+)
