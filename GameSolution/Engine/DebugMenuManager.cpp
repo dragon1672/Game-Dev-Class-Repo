7f7d5a4fb2802f984f36d98d1c189e39ce0b3f7b
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-03-13 00:13:38 +0000

    Moved function bodies to CPP file, prep to refactor main window with new Debug Menu
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@204 c70e0d21-08ce-ef44-a38d-91c21494a941

--- GameSolution/Engine/DebugMenuManager.cpp
+++ GameSolution/Engine/DebugMenuManager.cpp
@@ -1,1 +1,66 @@
 #include "DebugMenuManager.h"
+
+void DebugMenuManager::init(QLayout * toAdd) {
+	toAdd->addItem(&mainLayout);
+	mainLayout.addLayout(&floatWatchCol);
+	mainLayout.addLayout(&floatSlideCol);
+	mainLayout.addLayout(&boolCol);
+	mainLayout.addLayout(&vecWatchCol);
+	mainLayout.addLayout(&vecSlideCol);
+}
+void DebugMenuManager::update() {
+	for (int i = 0; i < floatWatchers.size(); i++) { floatWatchers[i]->update();          }
+	for (int i = 0; i < floatSliders.size();  i++) { floatSliders[i]->updateGUItoModel(); }
+	for (int i = 0; i < bools.size();         i++) { bools[i]->updateGUItoModel();        }
+	for (int i = 0; i < vecWatchers.size();   i++) { vecWatchers[i]->update();            }
+	for (int i = 0; i < vecSliders.size();    i++) { vecSliders[i]->updateGUItoModel();   }
+}
+void DebugMenuManager::watchFloat (char * name, float& toWatch)	  {
+	DebugMenuControllers::WatchFloatController * toAdd;
+	toAdd->init(name,&toWatch);
+	floatWatchers.push_back(toAdd);
+
+	QHBoxLayout * newRow = new QHBoxLayout();
+	newRow->addWidget(&(toAdd->label));
+	floatWatchCol.addLayout(newRow);
+}
+void DebugMenuManager::slideFloat (char * name, float& toWatch)	  {
+	DebugMenuControllers::SlideFloatController * toAdd;
+	toAdd->init(name,&toWatch);
+	floatSliders.push_back(toAdd);
+
+	QHBoxLayout * newRow = new QHBoxLayout();
+	newRow->addWidget(&(toAdd->label));
+	newRow->addWidget(&(toAdd->slider));
+	floatWatchCol.addLayout(newRow);
+}
+void DebugMenuManager::toggleBool (char * name, bool& toWatch)	  {
+	DebugMenuControllers::BoolToggleController * toAdd;
+	toAdd->init(name,&toWatch);
+	bools.push_back(toAdd);
+
+	QHBoxLayout * newRow = new QHBoxLayout();
+	newRow->addWidget(&(toAdd->checkBox));
+	boolCol.addLayout(newRow);
+}
+void DebugMenuManager::watchVector(char * name, glm::vec3& toWatch) {
+	DebugMenuControllers::WatchVectorController * toAdd;
+	toAdd->init(name,&toWatch);
+	vecWatchers.push_back(toAdd);
+
+	QHBoxLayout * newRow = new QHBoxLayout();
+	newRow->addWidget(&(toAdd->label));
+	vecWatchCol.addLayout(newRow);
+}
+void DebugMenuManager::slideVector(char * name, glm::vec3& toWatch, float min, float max) {
+	DebugMenuControllers::SlideVectorController * toAdd;
+	toAdd->init(name,&toWatch,min,max);
+	vecSliders.push_back(toAdd);
+
+	QHBoxLayout * newRow = new QHBoxLayout();
+	newRow->addWidget(&(toAdd->label));
+	newRow->addWidget(&(toAdd->xSlider));
+	newRow->addWidget(&(toAdd->ySlider));
+	newRow->addWidget(&(toAdd->zSlider));
+	vecSlideCol.addLayout(newRow);
+}
--- GameSolution/Engine/DebugMenuManager.h
+++ GameSolution/Engine/DebugMenuManager.h
@@ -26,70 +26,13 @@ private:
 
 	//stuff and things
 public:
-	void init(QLayout * toAdd) {
-		toAdd->addItem(&mainLayout);
-		mainLayout.addLayout(&floatWatchCol);
-		mainLayout.addLayout(&floatSlideCol);
-		mainLayout.addLayout(&boolCol);
-		mainLayout.addLayout(&vecWatchCol);
-		mainLayout.addLayout(&vecSlideCol);
-	}
-	void update() {
-		for (int i = 0; i < floatWatchers.size(); i++) { floatWatchers[i]->update();          }
-		for (int i = 0; i < floatSliders.size();  i++) { floatSliders[i]->updateGUItoModel(); }
-		for (int i = 0; i < bools.size();         i++) { bools[i]->updateGUItoModel();        }
-		for (int i = 0; i < vecWatchers.size();   i++) { vecWatchers[i]->update();            }
-		for (int i = 0; i < vecSliders.size();    i++) { vecSliders[i]->updateGUItoModel();   }
-	}
-	void watchFloat (char * name, float& toWatch)	  {
-		DebugMenuControllers::WatchFloatController * toAdd;
-		toAdd->init(name,&toWatch);
-		floatWatchers.push_back(toAdd);
-
-		QHBoxLayout * newRow = new QHBoxLayout();
-		newRow->addWidget(&(toAdd->label));
-		floatWatchCol.addLayout(newRow);
-	}
-	void slideFloat (char * name, float& toWatch)	  {
-		DebugMenuControllers::SlideFloatController * toAdd;
-		toAdd->init(name,&toWatch);
-		floatSliders.push_back(toAdd);
-
-		QHBoxLayout * newRow = new QHBoxLayout();
-		newRow->addWidget(&(toAdd->label));
-		newRow->addWidget(&(toAdd->slider));
-		floatWatchCol.addLayout(newRow);
-	}
-	void toggleBool (char * name, bool& toWatch)	  {
-		DebugMenuControllers::BoolToggleController * toAdd;
-		toAdd->init(name,&toWatch);
-		bools.push_back(toAdd);
-
-		QHBoxLayout * newRow = new QHBoxLayout();
-		newRow->addWidget(&(toAdd->checkBox));
-		boolCol.addLayout(newRow);
-	}
-	void watchVector(char * name, glm::vec3& toWatch) {
-		DebugMenuControllers::WatchVectorController * toAdd;
-		toAdd->init(name,&toWatch);
-		vecWatchers.push_back(toAdd);
-
-		QHBoxLayout * newRow = new QHBoxLayout();
-		newRow->addWidget(&(toAdd->label));
-		vecWatchCol.addLayout(newRow);
-	}
-	void slideVector(char * name, glm::vec3& toWatch, float min, float max) {
-		DebugMenuControllers::SlideVectorController * toAdd;
-		toAdd->init(name,&toWatch,min,max);
-		vecSliders.push_back(toAdd);
-
-		QHBoxLayout * newRow = new QHBoxLayout();
-		newRow->addWidget(&(toAdd->label));
-		newRow->addWidget(&(toAdd->xSlider));
-		newRow->addWidget(&(toAdd->ySlider));
-		newRow->addWidget(&(toAdd->zSlider));
-		vecSlideCol.addLayout(newRow);
-	}
+	void init(QLayout * toAdd);
+	void update();
+	void watchFloat (char * name, float& toWatch);
+	void slideFloat (char * name, float& toWatch);
+	void toggleBool (char * name, bool& toWatch);
+	void watchVector(char * name, glm::vec3& toWatch);
+	void slideVector(char * name, glm::vec3& toWatch, float min, float max);
 #else
 	void init(/*layout pos pointer*/) {}
 	void update() {}
