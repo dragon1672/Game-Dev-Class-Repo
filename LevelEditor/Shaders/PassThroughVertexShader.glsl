e792facf99fa1576f852ecfd02f7313c3a8beaa1
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-04-14 19:36:57 +0000

    Spheres now shaded to not be pure color
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@261 c70e0d21-08ce-ef44-a38d-91c21494a941

--- LevelEditor/Shaders/PassThroughVertexShader.glsl
+++ LevelEditor/Shaders/PassThroughVertexShader.glsl
@@ -17,7 +17,5 @@ uniform bool  enableOverrideColor;
 void main() {
 	vec4 transformedPos =  model2WorldTransform * vec4(pos.x,pos.y,pos.z,1);
 	gl_Position =  viewTransform * transformedPos;
-	//gl_Position =  vec4(pos.x,pos.y,pos.z,1);
-	//outColor = overrideColor * col;
 	outColor = enableOverrideColor? overrideColor : overrideColor * col;
 }
