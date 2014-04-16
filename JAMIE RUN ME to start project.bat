12349f94b248e7120fcb6fbbc8a1e390521ffd0b
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-04-16 01:55:40 +0000

    adding startup bat for jamie
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@266 c70e0d21-08ce-ef44-a38d-91c21494a941

---
+++ JAMIE RUN ME to start project.bat
@@ -0,0 +1,13 @@
+@echo off
+printf "+---------------------------------------------------------------+\n"
+printf "| controls                                                      |\n"
+printf "+-------------------+-------------------------------------------+\n"
+printf "| L Mouse Click     | Place or select nodes                     |\n"
+printf "| Shift + LMouse    | connect nodes                             |\n"
+printf "| Right Mouse Click | move camera                               |\n"
+printf "| Ctrl + 'A'        | show all connections                      |\n"
+printf "| Delete            | remove current node                       |\n"
+printf "+-------------------+-------------------------------------------+\n"
+
+pause
+start "" "LevelEditor\LevelEditor.sln"
