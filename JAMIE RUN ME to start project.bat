80b0867be8c3e381a2b784d885b66f7db0b017a0
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-12 23:25:26 +0000

    updating  bat file for new project
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@389 c70e0d21-08ce-ef44-a38d-91c21494a941

--- JAMIE RUN ME to start project.bat
+++ JAMIE RUN ME to start project.bat
@@ -1,26 +1,38 @@
 @echo off
+::echo "+---------------------------------------------------------------+"
+::echo "| controls                                                      |"
+::echo "+---------------------------------------------------------------+"
+::echo "|               Editor Mode                                     |"
+::echo "+-------------------+-------------------------------------------+"
+::echo "| L Mouse Click     | Place or select nodes                     |"
+::echo "| Shift + LMouse    | connect nodes/delete existing connection  |"
+::echo "| Right Mouse Click | move camera                               |"
+::echo "| Ctrl + 'A'        | show all connections                      |"
+::echo "| Delete            | remove current node                       |"
+::echo "| shift+Del         | remove all nodes                          |"
+::echo "+---------------------------------------------------------------+"
+::echo "|              Pathing Mode                                     |"
+::echo "+-------------------+-------------------------------------------+"
+::echo "| L Mouse Click     | selecte a node to path to                 |"
+::echo "| Right Mouse Click | Move cam if not attached                  |"
+::echo "+-------------------+-------------------------------------------+"
+
+::pause
+
+::LevelEditor\LevelEditor.sln
+::start "" "LevelEditor\LevelEditor.sln"
+
 echo "+---------------------------------------------------------------+"
 echo "| controls                                                      |"
 echo "+---------------------------------------------------------------+"
 echo "|               Editor Mode                                     |"
 echo "+-------------------+-------------------------------------------+"
-echo "| L Mouse Click     | Place or select nodes                     |"
-echo "| Shift + LMouse    | connect nodes/delete existing connection  |"
-echo "| Right Mouse Click | move camera                               |"
-echo "| Ctrl + 'A'        | show all connections                      |"
-echo "| Delete            | remove current node                       |"
-echo "| shift+Del         | remove all nodes                          |"
-echo "+---------------------------------------------------------------+"
-echo "|              Pathing Mode                                     |"
-echo "+-------------------+-------------------------------------------+"
-echo "| L Mouse Click     | selecte a node to path to                 |"
-echo "| Right Mouse Click | Move cam if not attached                  |"
+echo "| Click and Move    | Move position and vel of particle         |"
+echo "| Arrow Keys        | accelerate particle in arrow direction    |"
 echo "+-------------------+-------------------------------------------+"
 
-pause
 
-LevelEditor\LevelEditor.sln
-::start "" "LevelEditor\LevelEditor.sln"
+GamePhysics\GamePhysics.sln
 
 
 ::this will compile and run, find a better way to link to visual studio
