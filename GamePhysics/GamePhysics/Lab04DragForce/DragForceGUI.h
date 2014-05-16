6a566627ce2d197e3c85aa14b5980a159ae7a741
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-16 02:53:32 +0000

    Drag Forces Lab
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@398 c70e0d21-08ce-ef44-a38d-91c21494a941

---
+++ GamePhysics/GamePhysics/Lab04DragForce/DragForceGUI.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "../SingleParticleGui.h"
+#include <ParticleForceRegistry.h>
+#include <DragForceGenerator.h>
+#include <GravityForceGenerator.h>
+#include <VectorBoundForceGenerator.h>
+
+class DragForceGUI : public SingleParticleGui {
+	DragForceGenerator myDrag;
+	ParticleForceRegistry forceManager;
+	VectorGraphic * lowDragGraphic;
+	VectorGraphic * highDragGraphic;
+	glm::vec3 keyboardInput;
+	GravityForceGenerator myGrav;
+	VectorBoundForceGenerator myKeyboard;
+	bool enableGrav;
+	bool enableGrav_last;
+	bool enableKey;
+	bool enableKey_last;
+	bool enableDrag;
+	bool enableDrag_last;
+public:
+	void init() {
+		SingleParticleGui::init();
+		myParticle.init(1,1);
+		myDrag.init();
+		myGrav.dir = glm::vec3(0,-1,0);
+		myKeyboard.init(keyboardInput);
+
+		enableDrag = true;
+		enableDrag_last = false;
+
+		lowDragGraphic  = addVectorGraphic();
+		highDragGraphic = addVectorGraphic();
+		lowDragGraphic  -> displayStyle = DS_ARROW;
+		highDragGraphic -> displayStyle = DS_ARROW;
+		lowDragGraphic  -> color = glm::vec3(0,.7,0);
+		highDragGraphic -> color = glm::vec3(0,1,1);
+
+		resetParticle();
+		
+		//debug menus
+		myDebugMenu.slideFloat("Mass",myParticle.mass,  .1, 10);
+		myDebugMenu.slideFloat("Drag High",myDrag.high,   0,  5);
+		myDebugMenu.slideFloat("Drag Low", myDrag.low,    0,  5);
+		myDebugMenu.watchVector("Momentum",myParticle.momentum);
+		myDebugMenu.watchVector("Velocity",myParticle.vel);
+		myDebugMenu.watchVector("Position",myParticle.pos);
+		myDebugMenu.toggleBool("Enable Gravity",enableGrav);
+		myDebugMenu.toggleBool("Enable KeyBoard Force",enableKey);
+		myDebugMenu.toggleBool("Enable Drag Force",enableDrag);
+	}
+	void newFrame() {
+		SingleParticleGui::newFrame();
+		keyboardInput = getUserDirection() * 2.0f;
+		
+		updateForces();
+		forceManager.updateForces();
+
+		myParticle.update(dt());
+
+		particalGraphic->pointSize = myParticle.mass;
+
+		if(glm::dot(myParticle.vel,myParticle.vel)<.2
+			|| myParticle.vel.x < 0
+			|| myParticle.pos.x > 5
+			|| myParticle.pos.y < -2
+			|| myParticle.pos.y > 5
+			) {
+			resetParticle();
+		}
+
+		syncBaseVectors();
+		syncVector(lowDragGraphic, myDrag.lowForce, myParticle.pos);
+		syncVector(highDragGraphic,myDrag.highForce,myParticle.pos);
+	}
+private:
+	void updateForces() {
+		if(enableGrav != enableGrav_last) {
+			enableGrav ? forceManager.add(&myParticle,&myGrav) : forceManager.remove(&myParticle,&myGrav);
+		}
+		if(enableKey != enableKey_last) {
+			enableKey ? forceManager.add(&myParticle,&myKeyboard) : forceManager.remove(&myParticle,&myKeyboard);
+		}
+		if(enableDrag != enableDrag_last) {
+			enableDrag ? forceManager.add(&myParticle,&myDrag) : forceManager.remove(&myParticle,&myDrag);
+			myDrag.highForce = glm::vec3();
+			myDrag.lowForce = glm::vec3();
+		}
+		enableGrav_last = enableGrav;
+		enableKey_last  = enableKey;
+		enableDrag_last = enableDrag;
+	}
+	void resetParticle() {
+		myParticle.pos = glm::vec3(-3,3,0);
+		myParticle.vel = glm::vec3(8,0,0);
+	}
+};
--- GamePhysics/GamePhysics/Mainer.cpp
+++ GamePhysics/GamePhysics/Mainer.cpp
@@ -2,13 +2,16 @@
 #include "Lab01VA\BasicVelocityAndAcc.h"
 #include "Lab02FMA\BasicMomentum.h"
 #include "Lab03Force\ForceAccumulationGui.h"
+#include "Lab04DragForce\DragForceGUI.h"
 
 int main(int argc, char * argv[]) {
 	QApplication app(argc, argv);
 	
 	//BasicVelocityAndAcc widg;
 	//BasicMomentum widg;
-	ForceAccumulationGui widg;
+	//ForceAccumulationGui widg;
+	DragForceGUI widg;
+
 
 	widg.init();
 	widg.show();
