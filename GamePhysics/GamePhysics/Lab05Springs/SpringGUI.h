fe64a3158eb094404aad13e3f646c1f0b4a755f2
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-05-17 04:17:53 +0000

    double linked sliders
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@401 c70e0d21-08ce-ef44-a38d-91c21494a941

--- GamePhysics/GamePhysics/Lab05Springs/SpringGUI.h
+++ GamePhysics/GamePhysics/Lab05Springs/SpringGUI.h
@@ -17,36 +17,39 @@ class SpringGUI : public PhysicsGUIBase {
 	Particle two;
 	Particle thr;
 	Particle fur;
+	Particle fiv;
 	struct {
 		Particle * point;
 		VectorGraphic * pointGraphic;
 		VectorGraphic * velGraphic;
 		VectorGraphic * magGraphic;
-	} allPoints[4];
+	} allPoints[10];
 	int numOfPoints;
 	struct springRayStruct {
 		glm::vec3 * from;
 		glm::vec3 * to;
 		VectorGraphic * graphic;
-	} springRays[10];
+	} springRays[20];
 	int numOfSpringRays;
 public:
 	void init() {
 		PhysicsGUIBase::init();
 		myGrav.dir = glm::vec3();
-		mySprings.init(2,4);
+		mySprings.init(12,4);
 		myDrag.init(1,1);
 		myDebugMenu.slideFloat("Gravity", myGrav.dir.y,0,-50);
 		myDebugMenu.slideFloat("K", mySprings.springConstent,0,50);
+		myDebugMenu.slideFloat("Stiff K", mySprings.springConstent,0,500);
 		myDebugMenu.slideFloat("Rest", mySprings.springRestLength,0,10);
 
 		numOfPoints = 0;
 		numOfSpringRays = 0;
 
-		allPoints[numOfPoints++].point = &one;	one.pos = glm::vec3( 0,    0, 0);
-		allPoints[numOfPoints++].point = &two;	two.pos = glm::vec3(-2, -3.5, 0);
-		allPoints[numOfPoints++].point = &thr;	thr.pos = glm::vec3( 2, -3.5, 0);
-		allPoints[numOfPoints++].point = &fur;	fur.pos = glm::vec3( 0,   -7, 0);
+		allPoints[numOfPoints++].point = &one;	one.pos = glm::vec3( 0,   0, 0);
+		allPoints[numOfPoints++].point = &two;	two.pos = glm::vec3(-2, 3.5, 0);
+		allPoints[numOfPoints++].point = &thr;	thr.pos = glm::vec3( 2, 3.5, 0);
+		allPoints[numOfPoints++].point = &fur;	fur.pos = glm::vec3( 0,   7, 0);
+		allPoints[numOfPoints++].point = &fiv;	fiv.pos = glm::vec3( 0,  11, 0);
 		
 		for (int i = 0; i < numOfPoints; i++)
 		{
@@ -72,7 +75,9 @@ public:
 		mySprings.addSpring(two,fur.pos); { springRayStruct temp = {&two.pos, &fur.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; } 
 		mySprings.addSpring(fur,two.pos); { springRayStruct temp = {&fur.pos, &two.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; } 
 		mySprings.addSpring(thr,fur.pos); { springRayStruct temp = {&thr.pos, &fur.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; } 
-		mySprings.addSpring(fur,thr.pos); { springRayStruct temp = {&fur.pos, &thr.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; } // end double joints
+		mySprings.addSpring(fur,thr.pos); { springRayStruct temp = {&fur.pos, &thr.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; }
+		mySprings.addSpring(fur,fiv.pos); { springRayStruct temp = {&fur.pos, &fiv.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; } 
+		mySprings.addSpring(fiv,fur.pos); { springRayStruct temp = {&fiv.pos, &fur.pos, addVectorGraphic()}; springRays[numOfSpringRays++] = temp; } // end double joints 
 		for (int i = 0; i < numOfSpringRays; i++)
 		{
 			springRays[i].graphic->color = glm::vec3(1,.5,0);
@@ -99,5 +104,16 @@ public:
 	void vectorGraphicMouseDrag(uint vectorGraphicIndex, const glm::vec3& dragDelta) {
 		int index = vectorGraphicIndex / 2; // 2 graphics made for each point
 		allPoints[index].point->pos += dragDelta;
+
+		for (int i = 0; i < numOfPoints; i++)
+		{
+			syncVector(allPoints[i].pointGraphic,allPoints[i].point->pos);
+			syncVector(allPoints[i].velGraphic,allPoints[i].point->vel, allPoints[i].point->pos);
+		}
+		for (int i = 0; i < numOfSpringRays; i++)
+		{
+			glm::vec3 diff = *springRays[i].from - *springRays[i].to;
+			syncVector(springRays[i].graphic,diff,*springRays[i].to);
+		}
 	}
 };
