bcf2cace3192dbf733d4dc5ba2ba8b7be6b201d8
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-07-02 05:16:27 +0000

    Lab 1
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@462 c70e0d21-08ce-ef44-a38d-91c21494a941

--- ComputerGraphics/ComputerGraphics/labOne.h
+++ ComputerGraphics/ComputerGraphics/labOne.h
@@ -3,43 +3,63 @@
 #include <Engine\BasicGui.h>
 #include <ShapeGenerator.h>
 #include <Engine\WidgetRenderer.h>
+#include <Engine\Tools\NUShapeEditor.h>
 
 #include <glm\gtx\transform.hpp>
 
 class LabOne : public WidgetRenderer {
 public:
-	static const int skyboxScale = 2;
+	static const int skyboxScale = 4;
+	static const int cloudOffset = 1;
 	float skyboxRotation;
-	glm::mat4 * skyBoxMatrix;
+	float cloudRotation;
 
 	int alphaTexture;
 	int mainTexture;
 	int grassTexture;
+	int skyTexture;
+
+	Renderable * skyBoxRenderable;
+	Renderable * cloudRenderable;
+	Renderable * planeRenderable;
 	void init() {
-		skyboxRotation = 0;
+		myCam.setPos(glm::vec3(0,.1,0),glm::vec3(0,0,.5));
+
+		skyboxRotation = cloudRotation = 0;
 		auto meShader = addShader("../shaders/alphaV.glsl","../shaders/alphaF.glsl");
-		mainShader->buildBasicProgram("../shaders/passV.glsl","../shaders/passF.glsl");
 		saveViewTransform(meShader,"viewTransform");
+		mainShader = addShader("../shaders/passV.glsl","../shaders/passF.glsl");
+		saveViewTransform(mainShader,"viewTransform");
 
 		alphaTexture = addTexture("./../textures/Alpha.png");
-		mainTexture = addTexture("./../textures/Alpha_mainTexture.png");
+		mainTexture  = addTexture("./../textures/Alpha_mainTexture.png");
 		grassTexture = addTexture("./../textures/Grass.png");
+		skyTexture = addTexture("./../textures/nightSky.png");
 
-		auto skyBoxGeo = addGeometry(Neumont::ShapeGenerator::makeSphere(20),GL_TRIANGLES);
-		auto skyBoxRenderable = addRenderable(skyBoxGeo,meShader,mainTexture);
 
-		skyBoxMatrix = &skyBoxRenderable->whereMat;
+		
+
+		auto skyBoxGeo = addGeometry(NUShapeEditor::initUVData(Neumont::ShapeGenerator::makeSphere(20)),GL_TRIANGLES);
+		cloudRenderable = addRenderable(skyBoxGeo,meShader,mainTexture);
+		cloudRenderable->saveWhereMat("model2WorldTransform");
+		cloudRenderable->saveTexture("myTexture");
+		cloudRenderable->addUniformParameter("myAlpha",ParameterType::PT_TEXTURE,&alphaTexture);
+
+		skyBoxRenderable = addRenderable(skyBoxGeo,mainShader,skyTexture);
 		skyBoxRenderable->saveWhereMat("model2WorldTransform");
 		skyBoxRenderable->saveTexture("myTexture");
-		skyBoxRenderable->addUniformParameter("myAlpha",ParameterType::PT_TEXTURE,&alphaTexture);
 
 		auto planeGeo = addGeometry(Neumont::ShapeGenerator::makePlane(10),GL_TRIANGLES);
-		auto planeRenderable = addRenderable(planeGeo,mainShader,grassTexture);
+		planeRenderable = addRenderable(planeGeo,mainShader,grassTexture);
 		planeRenderable->saveWhereMat("model2WorldTransform");
 		planeRenderable->saveTexture("myTexture");
 	}
 	void nextFrame(float dt) {
-		skyboxRotation += dt;
-		*skyBoxMatrix = glm::rotate(skyboxRotation,glm::vec3(0,0,1)) * glm::scale(glm::vec3(skyboxScale,skyboxScale,skyboxScale));
+		cloudRotation -= 10 * dt;
+		skyboxRotation += 3 * dt;
+		cloudRenderable->whereMat = glm::rotate(cloudRotation,glm::vec3(0,0,1)) * glm::scale(glm::vec3(skyboxScale,skyboxScale,skyboxScale));
+		skyBoxRenderable->whereMat = glm::rotate(skyboxRotation,glm::vec3(0,0,1)) * glm::scale(glm::vec3(skyboxScale+cloudOffset,skyboxScale+cloudOffset,skyboxScale + cloudOffset));
+		planeRenderable->whereMat = glm::scale(glm::vec3(1,1,1));
+		//*skyBoxMatrix = glm::rotate(skyboxRotation,glm::vec3(0,0,1)) * glm::scale(glm::vec3(skyboxScale,skyboxScale,skyboxScale));
 	}
 };
