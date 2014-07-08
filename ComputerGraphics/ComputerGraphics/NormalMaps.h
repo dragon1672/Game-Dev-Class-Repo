5171c8dbe0d8ffad215beb9541f115bf45264315
acorbin <acorbin@c70e0d21-08ce-ef44-a38d-91c21494a941>
2014-07-08 22:05:20 +0000

    Basic Normal lab
    git-svn-id: https://svn.neumont.edu:8443/svn/cplusplus_anthony_corbin@470 c70e0d21-08ce-ef44-a38d-91c21494a941

--- ComputerGraphics/ComputerGraphics/NormalMaps.h
+++ ComputerGraphics/ComputerGraphics/NormalMaps.h
@@ -24,28 +24,33 @@ public:
 		myCam.setPos(glm::vec3(0,.1,30),glm::vec3(0,0,-.5));
 
 		specPower = 100;
+		lightColor = glm::vec3(1,1,1);
+		lightSrc = glm::vec3(0,4,10);
 
-		auto meGeo =  addGeometry(Neumont::ShapeGenerator::makePlane(10),GL_TRIANGLES);
+		
+
+		auto meGeo =  addGeometry(NUShapeEditor::scale(BinaryToShapeLoader::loadFromFile("./../models/plane.bin"),5),GL_TRIANGLES);
 		auto cameraGeo = addGeometry(Neumont::ShapeGenerator::makeCube(),GL_TRIANGLES);
 
 		lightSrcRenderable = addRenderable(cameraGeo,mainShader,-1);
 		lightSrcRenderable->saveWhereMat("model2WorldTransform");
 
-		ShaderProgram * meShader = addShader("./../shaders/basicNormalV.glsl","./../shaders/basicNormalF.glsl"); // for clock, clock face
+		ShaderProgram * meShader = addShader("./../shaders/basicNormal_V.glsl","./../shaders/basicNormal_F.glsl"); // for clock, clock face
 		saveViewTransform(meShader,"viewTransform");
-		meShader->saveUniform("lightSrc",   ParameterType::PT_VEC3, &lightSrc[0]       );
+		meShader->saveUniform("diffusePos",   ParameterType::PT_VEC3, &lightSrc[0]     );
 		meShader->saveUniform("lightColor", ParameterType::PT_VEC3, &lightColor[0]     );
 		meShader->saveUniform("camPos",     ParameterType::PT_VEC3, &myCam.getPos()[0] );
 		
 		mePlane = addRenderable(meGeo,meShader,addTexture("./../textures/basicNormal.png"));
 		mePlane->saveTexture("normalMap");
 		mePlane->saveWhereMat("model2WorldTransform");
-		mePlane->whereMat = glm::rotate(180.0f,glm::vec3(1,0,0));
+		mePlane->whereMat = glm::rotate(90.0f,glm::vec3(1,0,0));
+							//* glm::rotate(-90.0f,glm::vec3(0,0,1));
 
 
 
-		menu->edit("lightPosition",lightSrc,1,200);
-		menu->edit("lightColor",specPower,1,200);
+		menu->edit("lightPosition",lightSrc,-15,15);
+		menu->edit("lightColor",lightColor,0,1);
 		menu->edit("specPower",specPower,1,200);
 		menu->edit("showLightSrc",lightSrcRenderable->visible);
 	}
--- ComputerGraphics/shaders/basicNormal_V.glsl
+++ ComputerGraphics/shaders/basicNormal_V.glsl
@@ -4,6 +4,8 @@ in layout(location=0) vec3 pos;
 in layout(location=3) vec2 uv;
 
 out vec2 outUv;
+out vec4 outCol;
+out vec3 outPos;
 
 //mats
 uniform mat4x4 viewTransform;
@@ -12,5 +14,7 @@ uniform mat4x4 model2WorldTransform;
 void main() {
 	vec4 transformedPos =  model2WorldTransform * vec4(pos.x,pos.y,pos.z,1);
 	gl_Position =  viewTransform * transformedPos;
+	outPos = vec3(transformedPos);
 	outUv = uv;
+	outCol = vec4(1,1,1,1);
 }
